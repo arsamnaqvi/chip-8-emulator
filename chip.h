240054a6517db1dbca8fedcb00986e7f0e6e9d3f
Arsam <arsamhassan1999@gmail.com>
2021-08-17 20:13:30 -0500

    chip architecture added

---
+++ chip.h
@@ -0,0 +1,27 @@
+#include <stdint.h>
+
+// macros for the class used below
+#define NUM_REG     16
+#define MEM_SIZE    4096
+#define STACK_SIZE  16
+#define KEYPAD_SIZE 16
+#define VIDMEM_SIZE 2048    // 64*32
+
+/* class definition for the chip8 infrastructure 
+ * the names are self explanatory and represent 
+ * exactly what they say
+ */
+class chip8 {
+    public:
+        uint8_t registers[NUM_REG]{};
+        uint8_t memory[MEM_SIZE]{};
+        uint16_t index{};
+        uint16_t pc{};
+        uint16_t stack[STACK_SIZE]{};
+        uint8_t sp{};
+        uint8_t delaytimer{};
+        uint8_t soundtimer{};
+        uint8_t keypad[KEYPAD_SIZE]{};
+        uint32_t video[VIDMEM_SIZE]{};
+        uint16_t opcode;
+};
